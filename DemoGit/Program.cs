0ea1b44dd88950f83a6fd48a157cb9fa0c48ec46
Carl Sporrong <carl.sporrong@gmail.com>
2024-04-16 15:15:48 +0200

    Updated

--- DemoGit/Program.cs
+++ DemoGit/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, Olle här!");
+            Console.WriteLine("Hello, Carl här igen!");
         }
     }
 }
